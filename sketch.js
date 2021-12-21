6b5e606ca1d477f88ff59f9a1b2c33c6d1e93934
jeevikabenki <96451825+jeevikabenki@users.noreply.github.com>
2021-12-20 19:04:35 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+
+
+
+function setup() {
+  createCanvas(400,400);
+  var box = createSprite(200,200,20,20);
+  box.shapeColor = "blue";
+
+}
+
+
+
+
+function draw() 
+{
+  background(30);
+drawSprites();
+}
+
+
+
+
